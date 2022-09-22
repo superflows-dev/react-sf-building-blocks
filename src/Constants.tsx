418a8d6e0ed427cc8840ca2acd858db992bd1445
Superflows <108924653+Superflow-3@users.noreply.github.com>
2022-09-22 16:44:51 +0530

    Constants updated

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -1,8 +1,8 @@
 const Constants = {
 
-    DEFAULT_BRAND_ICON: "https://superflows.dev/img/superflows_gray_transparent_200.png",
+    DEFAULT_BRAND_ICON: "",
     DEFAULT_BRAND_NAME: "Superflows",
-    DEFAULT_MENU: [{caption: "About", link: "about"}, [{caption: "Solutions", link: "solutions"}, {caption: "Products", link: "products"}, {caption: "Services", link: "services"}, {caption: "Resources", link: "resources"}], {caption: "Team", link: "team"}, [{caption: "Contact", link: "contact"}, {caption: 'Instagram', link: "instagram"}, {caption: "Facebook", link: "facebook"}]]
+    DEFAULT_MENU: [{caption: "About", link: "about"}, [{caption: "Solutions", link: "solutions"}, {caption: "Products", link: "products"}, {caption: "Services", link: "services"}, {caption: "Resources", link: "resources"}], [{caption: "Contact", link: "contact"}, {caption: 'Instagram', link: "instagram"}, {caption: "Facebook", link: "facebook"}]]
 
 }
 
