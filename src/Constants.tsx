87fd1d5d68ea30003d712daed42c8c044a59d9f5
Superflows <108924653+Superflow-3@users.noreply.github.com>
2022-12-03 09:31:18 +0530

    SfNav: Added optional mobile properties for banner

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -6,7 +6,9 @@ const Constants = {
     DEFAULT_MENU: [{caption: "About", link: "about"}, [{caption: "Solutions", link: "solutions"}, {caption: "Products", link: "products"}, {caption: "Services", link: "services"}, {caption: "Resources", link: "resources"}], [{caption: "Contact", link: "contact"}, {caption: 'Instagram', link: "instagram"}, {caption: "Facebook", link: "facebook"}]],
     DEFAULT_NOTIFICATION_LIST: [{id: 1, title: "First one!", description: 'This is the description of the firs notification.', timestampReceived: "2 days ago", read: false}, {id: 2, title: "2nd notification", description: 'Displaying notifications is super simple!', timestampReceived: "5 days ago", read: true}, {id: 3, title: "Get started now", description: 'Create the notification list using a simple JSON array and your job is done.', timestampReceived: "1 month ago", read: false}],
     DEFAULT_BANNER_TEXT: "⭐️ Give us a star on Github!",
+    DEFAULT_BANNER_TEXT_MOBILE: "⭐️ Give us a star!",
     DEFAULT_BANNER_CTA: "View on GitHub",
+    DEFAULT_BANNER_CTA_MOBILE: "GitHub",
 
 }
 
